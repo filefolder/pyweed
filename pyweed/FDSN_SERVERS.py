a902302c6c9a0eb922a1f6445d6569705bb5d3f3
filefolder <rpickle@gmail.com>
2021-04-04 12:20:50 +1000

    Add files via upload
    new file to optionally add FDSN servers which may not be included in ObsPy by default

---
+++ pyweed/FDSN_SERVERS.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Add FDSN URL's to those listed by default in obspy.clients.fdsn.header.URL_MAPPINGS 
+
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+from obspy.clients.fdsn.header import URL_MAPPINGS
+
+MORE_FDSN = {'AUSPASS':'http://auspass.edu.au',
+             'BATS':'http://batsws.earth.sinica.edu.tw',
+             'CERI':'https://www.memphis.edu/ceri'}
+
+URL_MAPPINGS.update(MORE_FDSN)
