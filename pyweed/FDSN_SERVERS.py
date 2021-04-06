513a012b3b29d54972da1f99798ed9cdfe419ee6
filefolder <rpickle@gmail.com>
2021-04-06 15:57:01 +1000

    Update FDSN_SERVERS.py

--- pyweed/FDSN_SERVERS.py
+++ pyweed/FDSN_SERVERS.py
@@ -10,6 +10,6 @@ from obspy.clients.fdsn.header import URL_MAPPINGS
 
 MORE_FDSN = {'AUSPASS':'http://auspass.edu.au',
              'BATS':'http://batsws.earth.sinica.edu.tw',
-             'CERI':'https://www.memphis.edu/ceri'}
+             'LOCALHOST':'http://localhost:8080'}
 
 URL_MAPPINGS.update(MORE_FDSN)
